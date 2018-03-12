28a745a76f8b9d693437620e5595d5cf4b90ab9c
Kristiana Papajani <papajani.kristiana@gmail.com>
2018-03-11 23:28:45 -0400

    Add files via upload

---
+++ econalyzeThemePlugin.inc.php
@@ -0,0 +1,18 @@
+<?php
+import('lib.pkp.classes.plugins.ThemePlugin');
+class econalyzeThemePlugin extends ThemePlugin {
+  
+  public function init() {
+    $this->setParent('defaultthemeplugin');
+    $this->modifyStyle('stylesheet', array('addLess' => array('styles/variables.less','styles/custom.less')));
+  }
+  
+  public function getDisplayName() {
+    return 'econalyze child theme';
+  }
+  
+  public function getDescription() {
+    return 'This is a custom theme developed by the digital projects team at York University Libraries.';
+  }
+}
+?> 
